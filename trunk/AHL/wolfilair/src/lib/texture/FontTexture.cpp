b9bcac21e1aa3730421b96831159e175b9f0826d
xpierro@gmail.com <xpierro@gmail.com@83c105a5-504d-a500-87cf-ba6e0fdb11ff>
2010-10-19 14:24:43 +0000

    Conversion from string to texture with custom TTF font is now possible, however since OpenGL use the beginning of a texture buffer as the bottom left corner, the texture seems upside down. A flipping is necessary to ensure coherency with other textures.

---
+++ trunk/AHL/wolfilair/src/lib/texture/FontTexture.cpp
@@ -0,0 +1,21 @@
+/*
+ * FontTexture.cpp
+ *
+ *  Created on: 19 oct. 2010
+ *      Author: Pierre
+ */
+
+#include "FontTexture.h"
+
+namespace PS3 {
+
+FontTexture::FontTexture() {
+	// TODO Auto-generated constructor stub
+
+}
+
+FontTexture::~FontTexture() {
+	// TODO Auto-generated destructor stub
+}
+
+}
